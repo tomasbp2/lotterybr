6e0e917d1b680a0852ca1f7251a92a2577f35c14
Tomás Bernardes <150491378+tomasbp2@users.noreply.github.com>
2024-04-08 19:53:00 -0300

    Add files via upload

---
+++ R/get_data.R
@@ -0,0 +1,29 @@
+#'#' Function to download lottery data from git repository
+
+#' @export
+#'
+#' @param game select which Brazilian lottery game
+#' @param type select if you want the winners database or the results
+
+#' @return tibble/data.frame containing the downloaded data
+
+#' @description This function downloads the Brazilian lottery game data from Caixa Federal up to the date declared
+
+#' @examples
+#' \dontrun{
+#' library(LotteryBrasil)
+#' megasena_ganhadores <- get_data(game= "megasena",type = "ganhadores")
+#' megasena_ganhadores
+#'
+#' lotofacil_dezenas <- get_data(game= "lotofacil",type = "dezenas")
+#' lotofacil_dezenas
+#' }
+#'
+
+get_data = function(game= c("maismilionaria", "megasena", "lotofacil",  "quina", "lotomania",  "timemania", "duplasena",  "federal", "loteca", "diadesorte", "supersete"),
+                    type = c("dezenas","ganhadores")){
+
+  url = paste0("https://github.com/tomasbp2/LotteryBrasilDATA/blob/main/",game,"/",type,".rds?raw=true")
+  file = readRDS(url(url))
+  return(file)
+}
